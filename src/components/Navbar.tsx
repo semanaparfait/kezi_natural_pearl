ce54cf2c7d84e8a97061d1e69efe44207712af2c
semanaparfait <p.semana@alustudent.com>
2026-01-26 10:58:47 +0200

    resolving error react declared but not used

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 import Button from '@/components/Button';
 import { useLocation } from 'react-router-dom';
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function ProductCard() {
   return (
     <div>ProductCard</div>
